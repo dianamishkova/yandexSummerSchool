c639c35029004a9155a290ebb5a6d523c18d9b5c
Диана Мишкова <dmshk@MacBook-Air-Diana-2.local>
2024-06-22 01:38:52 +0300

    added homework1 for cross-check

---
+++ ToDoList/ToDoList/FileCache.swift
@@ -0,0 +1,49 @@
+//
+//  FileCache.swift
+//  ToDoList
+//
+//  Created by Диана Мишкова on 18.06.24.
+//
+
+import Foundation
+
+class FileCache {
+    private(set) var todoItemsList: [TodoItem] = []
+    
+    func addItem(_ todoItem: TodoItem) {
+        if !todoItemsList.contains(where: { $0.id == todoItem.id }) {
+            todoItemsList.append(todoItem)
+        }
+    }
+    
+    func deleteItem(id: String) {
+        todoItemsList.removeAll { $0.id == id }
+    }
+    
+    func save(to fileName: String) throws {
+        let jsonObject = todoItemsList.map { $0.json }
+        let jsonData = try JSONSerialization.data(withJSONObject: jsonObject, options: [])
+        let url = getFileURL(fileName: fileName)
+        try jsonData.write(to: url)
+    }
+    
+    func load(fromJSON fileName: String) throws {
+        let url = getFileURL(fileName: fileName)
+        let jsonData = try Data(contentsOf: url)
+        guard let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [Any] else { return }
+        todoItemsList = jsonObject.compactMap { TodoItem.parse(json: $0) }
+    }
+    
+    func load(fromCSV fileName: String) throws {
+        let url = getFileURL(fileName: fileName)
+        let csvString = try String(contentsOf: url)
+        let rows = csvString.components(separatedBy: "\n").dropFirst()
+        todoItemsList = rows.compactMap { TodoItem.parse(csv: $0) }
+    }
+    
+    private func getFileURL(fileName: String) -> URL {
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        return urls[0].appendingPathComponent(fileName)
+    }
+}
